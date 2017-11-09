c9dd7b46b76e415ec8e2e72f3466e9c12636ee79
Gergo <halmi.gergo@gmail.com>
2017-11-09 18:59:22 +0100

    new window and dev tool added in non-production mode

--- index.js
+++ index.js
@@ -3,25 +3,42 @@ const electron = require("electron")
 const {app, BrowserWindow, Menu} = electron
 
 let mainWindow // init the main window variable, this solves some scoping issue
+let addWindow
 
 app.on('ready', () => {
   console.log('app is ready')
   mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/main.html`)
+  mainWindow.on('closed', () => app.quit())
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(mainMenu)
 
 })
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add New Todo'
+  })
+  addWindow.loadURL(`file://${__dirname}/add.html`)
+}
+
 const menuTemplate = [
    {
      label: 'File', // this appear as the app name on OSX
      submenu: [
+       {
+         label: 'New Todo',
+         click() {
+           createAddWindow()
+         }
+       },
        { label: 'Settings' },
        {
          label: 'Quit',
-         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q'
+         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
          click() {
            app.quit()
          }
@@ -29,3 +46,18 @@ const menuTemplate = [
      ]
    }
 ]
+
+if(process.env.NODE_ENV  !== 'production') {
+  menuTemplate.push({
+    label: 'DEVELOPER',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+D' : 'Ctrl+D',
+        click(item, focusedWindow) {
+            focusedWindow.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
