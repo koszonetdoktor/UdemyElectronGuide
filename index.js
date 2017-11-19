5a1b9c7466ceeb0a072aac4e2adaff4d066652e9
Gergo <halmi.gergo@gmail.com>
2017-11-19 18:29:13 +0100

    project setup, react pullin, tray added, sstarted with bounds positioning

---
+++ index.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+const path = require('path')
+
+const { app, BrowserWindow, Tray } = electron
+
+let mainWindow
+let tray
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false, //this makes the statusbar disappear
+    resizable: false, // dont allow the user to resize the window
+    show: false //  the bowser window initially not visible
+  })
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+  tray = new Tray(iconPath)
+  tray.on('click', (event, bounds) => {
+    console.log(bounds)
+    if(mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show()
+      //mainWindow.
+    }
+  })
+})
